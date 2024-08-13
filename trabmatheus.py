00b87c3d6fb35d604ee70a0e39c80e94f41835db
Luiza Novais Tavares <gbntavares@gmail.com>
2024-08-13 12:22:58 -0300

    Add sorteia_e_calcula_media function

--- trabmatheus.py
+++ trabmatheus.py
@@ -2,3 +2,14 @@ def cumprimento(texto):
     return f"Olá, {texto}"
 
 print(cumprimento("Gabriel Novais"))
+
+
+import random
+
+def sorteia_e_calcula_media():
+    numeros = [random.randint(1, 100) for _ in range(3)]
+    media = sum(numeros) / len(numeros)
+    return media
+
+media_numeros = sorteia_e_calcula_media()
+print(f"A média dos três números sorteados é: {media_numeros}")
